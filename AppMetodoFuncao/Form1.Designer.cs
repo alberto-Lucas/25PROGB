2218155fe63081891fd2f99fa595b79bb6a8521c
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-03-06 22:27:52 -0300

    AppMetodoFuncao aula dia 06/03/2025

---
+++ AppMetodoFuncao/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AppMetodoFuncao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbOpcoes = new System.Windows.Forms.ComboBox();
+            this.btnIF = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnMetodo = new System.Windows.Forms.Button();
+            this.btnFuncao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Opção:";
+            // 
+            // cbbOpcoes
+            // 
+            this.cbbOpcoes.FormattingEnabled = true;
+            this.cbbOpcoes.Items.AddRange(new object[] {
+            "MC FISH",
+            "BIG TASTY",
+            "BIG MAC",
+            "QUARTEIRAO"});
+            this.cbbOpcoes.Location = new System.Drawing.Point(18, 31);
+            this.cbbOpcoes.Name = "cbbOpcoes";
+            this.cbbOpcoes.Size = new System.Drawing.Size(121, 21);
+            this.cbbOpcoes.TabIndex = 1;
+            // 
+            // btnIF
+            // 
+            this.btnIF.Location = new System.Drawing.Point(18, 58);
+            this.btnIF.Name = "btnIF";
+            this.btnIF.Size = new System.Drawing.Size(75, 23);
+            this.btnIF.TabIndex = 2;
+            this.btnIF.Text = "IF";
+            this.btnIF.UseVisualStyleBackColor = true;
+            this.btnIF.Click += new System.EventHandler(this.btnIF_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(18, 87);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 3;
+            this.btnSwitch.Text = "SWITCH";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor 1:";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(204, 37);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 20);
+            this.txtValor1.TabIndex = 5;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(204, 87);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(100, 20);
+            this.txtValor2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valor2:";
+            // 
+            // btnMetodo
+            // 
+            this.btnMetodo.Location = new System.Drawing.Point(334, 37);
+            this.btnMetodo.Name = "btnMetodo";
+            this.btnMetodo.Size = new System.Drawing.Size(75, 23);
+            this.btnMetodo.TabIndex = 8;
+            this.btnMetodo.Text = "METODO";
+            this.btnMetodo.UseVisualStyleBackColor = true;
+            this.btnMetodo.Click += new System.EventHandler(this.btnMetodo_Click);
+            // 
+            // btnFuncao
+            // 
+            this.btnFuncao.Location = new System.Drawing.Point(334, 66);
+            this.btnFuncao.Name = "btnFuncao";
+            this.btnFuncao.Size = new System.Drawing.Size(75, 23);
+            this.btnFuncao.TabIndex = 9;
+            this.btnFuncao.Text = "FUNCAO";
+            this.btnFuncao.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(504, 170);
+            this.Controls.Add(this.btnFuncao);
+            this.Controls.Add(this.btnMetodo);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnIF);
+            this.Controls.Add(this.cbbOpcoes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbOpcoes;
+        private System.Windows.Forms.Button btnIF;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnMetodo;
+        private System.Windows.Forms.Button btnFuncao;
+    }
+}
+
---
+++ AppMetodoFuncao/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMetodoFuncao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIF_Click(object sender, EventArgs e)
+        {
+            //Ao clicar no botão, iremos apresentar
+            //a opção numerica que o usuario
+            //selecionou
+
+            string opcao;
+
+            //Fazer a cascata de IF
+            //utilizado quando podemos ter condições
+            //diferentes
+
+            //O combobox é nada mais doque um Array
+            //começando posição 0
+            //Ou seja cada item possui um Index = Id da posição
+
+            //Estou pegando o Indice Selecionado do Array
+            if (cbbOpcoes.SelectedIndex == 0)
+                opcao = "Opção 1 Selecionada.";
+            else if (cbbOpcoes.SelectedIndex == 1)
+                opcao = "Opção 2 Selecionada.";
+            else if (cbbOpcoes.SelectedIndex == 2)
+                opcao = "Opção 3 Selecionada.";
+            else if (cbbOpcoes.SelectedIndex == 3)
+                opcao = "Opção 4 Selecionada.";
+            else
+                opcao = "Nenhuma opção selecionada";
+            //Exibir o texto para o usuario
+            MessageBox.Show(opcao);
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            //seguir o principio do botao IF
+
+            //Switch é utilizado qunado possui
+            //variação fixa
+            //ou seja tenho uma varição de um tipo de dados
+
+            string opcao;
+
+            switch(cbbOpcoes.SelectedIndex)
+            {
+                case 0: opcao = "Opção 1 Selecionada";
+                    break;
+                case 1: opcao = "Opção 2 Selecionada";
+                    break;
+                case 2: opcao = "Opção 3 Selecionada";
+                    break;
+                case 3: opcao = "Opção 4 Selecionada";
+                    break;
+                default: opcao = "Nenuma Opção Selecionada";
+                    break;
+            }
+
+            MessageBox.Show(opcao);
+        }
+
+        private void btnMetodo_Click(object sender, EventArgs e)
+        {
+            //Chamo o método pelo nome
+            MetodoSomar();
+        }
+
+        //Como criar metodo
+        //Definir o tipo de retorno
+        //Metódo não possui retorno portanto
+        //sera do tipo VOID
+        //VOID = sem retorno
+
+        //Void = Tipo
+        //MetodoSomear = Nome do metodo
+        void MetodoSomar()
+        {
+            //Um método vai executar um bloco de
+            //código
+
+            //Vamos definir as variaveis
+            int v1, v2, resultado;
+
+            //Atribuir o valor da campo em 
+            //em cada variavel valor
+
+            //Converter o texto em inteiro
+            //definir o tipo de dado final
+            //e chamar a conversão
+            v1 = int.Parse(txtValor1.Text);
+            v2 = int.Parse(txtValor2.Text);
+
+            resultado = v1 + v2;
+
+            MessageBox.Show("Resultado: " + 
+                            resultado.ToString());
+        }
+
+    }
+}
